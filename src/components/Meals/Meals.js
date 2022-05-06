3595616aa35db933958915f4e1e55114fd05f8aa
keatonjwhitehead <keaton.j.whitehead@gmail.com>
2022-05-06 13:22:20 -0600

    Display list of foods through maping each MealItem

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+import { Fragment } from 'react/cjs/react.production.min';
+
+const Meals = () => {
+    return (
+        <Fragment>
+            <MealsSummary />
+            <AvailableMeals />
+        </Fragment>
+    )
+}
+
+export default Meals;
