213dabc66f860bc5fc98f97fae2cdda77820cc46
keatonjwhitehead <keaton.j.whitehead@gmail.com>
2022-05-06 13:58:26 -0600

    Update the food item list and added food item form\

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import classes from './AvailableMeals.module.css';
-import MealItem from './MealItem';
+import MealItem from './MealItem/MealItem';
+import Card from '../UI/Card';
 const DUMMY_MEALS = [
     {
       id: 'm1',
@@ -28,12 +29,22 @@ const DUMMY_MEALS = [
   ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <MealItem id={meal.id} name={meal.name} description={meal.description} price={meal.price}></MealItem>);
+    const mealsList = DUMMY_MEALS.map(meal => (
+    <MealItem 
+        key={meal.id}
+        name={meal.name}
+        description={meal.description} 
+        price={meal.price} 
+    />
+    ));
 
     return <section className={classes.meals}>
+        <Card>
         <ul>
             {mealsList}
         </ul>
+        </Card>
+
     </section>
 }
 
